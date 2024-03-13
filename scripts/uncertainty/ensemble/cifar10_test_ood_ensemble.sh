47a0a55f4f0f4a010e0ead5f815ed25d9431916a
Pritom <pritomkumarsaha@live.com>
2024-03-13 09:16:04 +0000

    ensemble script updated

--- scripts/uncertainty/ensemble/cifar10_test_ood_ensemble.sh
+++ scripts/uncertainty/ensemble/cifar10_test_ood_ensemble.sh
@@ -14,15 +14,15 @@ PYTHONPATH='.':$PYTHONPATH \
 python main.py \
 --config configs/datasets/cifar10/cifar10.yml \
 configs/datasets/cifar10/cifar10_ood.yml \
-configs/networks/resnet18_32x32.yml \
+configs/networks/wrn.yml \
 configs/pipelines/test/test_ood.yml \
 configs/preprocessors/base_preprocessor.yml \
 configs/postprocessors/ensemble.yml \
 --network.pretrained False \
 --num_workers 8 \
 --mark 0 \
---postprocessor.postprocessor_args.network_name resnet18_32x32 \
---postprocessor.postprocessor_args.checkpoint_root 'results/cifar10_resnet18_test_ensemble' \
+--postprocessor.postprocessor_args.network_name wrn \
+--postprocessor.postprocessor_args.checkpoint_root 'results/cifar10_wrn_base_e100_lr0.1_default' \
 --postprocessor.postprocessor_args.num_networks 5 \
 --dataset.test.batch_size 64 \
 --dataset.val.batch_size 64 \
